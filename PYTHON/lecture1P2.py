d9cfaca9a42eacf85d9a63cfd3e46c197f51221c
aayushguta-art <aayushgupta676767@gmail.com>
2026-09-19 17:52:59 +0530

    Imported pyttsx3 and successfully converted text into speech

--- PYTHON/lecture1P2.py
+++ PYTHON/lecture1P2.py
@@ -4,5 +4,5 @@ engine = pyttsx3.init()
 # For Mac, If you face error related to "pyobjc" when running the `init()` method :
 # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
 
-engine.say("I will speak this text")
+engine.say("Aayush is my master and he will conquer the world")
 engine.runAndWait()
